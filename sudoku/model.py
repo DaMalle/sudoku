36f3ce18ce76c3e041cfc99f6a21d349d102d265
DaMalle <marcfriis@protonmail.com>
2025-06-15 21:06:56 +0200

    fixed spelling

--- sudoku/model.py
+++ sudoku/model.py
@@ -19,12 +19,16 @@ class SudokuModel:
 
 class SolutionGenerator:
     def create(self) -> tuple[int, ...]:
-        """Creates a valid sudoku solution as a 1D tuple of 81 elements, where each group of 9 represents a row."""
+        """
+        Returns a valid sudoku solution as a 1D tuple of 81 elements,
+        where each group of 9 represents a row.
+        """
 
         n = [0, 1, 2]
 
-        # Column groups are [(0, 1, 2), (3, 4, 5), (6, 7, 8)], split based on which box the column is in.
-        # column order shuffles the order of the column within each group and the order of the groups
+        # Column groups are [ 0, 1, 2 | 3, 4, 5 | 6, 7, 8 ], split based on which box the column is in.
+        # column_order shuffles the order of the column within each group and the order of the groups
+        # Example: [ 2, 1, 0 | 7, 6, 8 | 4, 3, 5 ]
         column_order = tuple(group * 3 + column
             for group in self._shuffle(n)
             for column in self._shuffle(n)
