21bff8e92afcfc1d04ccf67e42749f36004925f6
DaMalle <marcfriis@protonmail.com>
2025-06-15 20:56:43 +0200

    refactored SolutionGenerator in model.py

--- sudoku/model.py
+++ sudoku/model.py
@@ -3,7 +3,7 @@ import random
 
 class SudokuModel:
     def __init__(self) -> None:
-        self._solution_generator = SolutionGenerator()
+        self._solution_generator = SolutionGenerator
 
         self._solution: tuple[int, ...] | None = None
         self._game_board: list[int] | None = None
@@ -18,34 +18,37 @@ class SudokuModel:
 
 
 class SolutionGenerator:
-    def __init__(self) -> None:
-        self._BOX_SIDE = 3
-        self._GRID_SIDE = 9
-        self._GRID = 81
-
     def create(self) -> tuple[int, ...]:
-        _shuffle = lambda nums: random.sample(nums, len(nums))
+        """Creates a valid sudoku solution as a 1D tuple of 81 elements, where each group of 9 represents a row."""
 
         n = [0, 1, 2]
-        column_order = tuple(g * 3 + c
-            for g in _shuffle(n)
-            for c in _shuffle(n)
+
+        # Column groups are [(0, 1, 2), (3, 4, 5), (6, 7, 8)], split based on which box the column is in.
+        # column order shuffles the order of the column within each group and the order of the groups
+        column_order = tuple(group * 3 + column
+            for group in self._shuffle(n)
+            for column in self._shuffle(n)
         )
 
-        row_order = tuple(g * 3 + r
-            for g in _shuffle(n)
-            for r in _shuffle(n)
+        row_order = tuple(group * 3 + row
+            for group in self._shuffle(n)
+            for row in self._shuffle(n)
         )
 
-        _apply_pattern = lambda r, c: (3 * (r % 3) + r // 3 + c) % 9
+        pattern = lambda r, c: (3 * (r % 3) + r // 3 + c) % 9
+        seed = self._shuffle(list(range(1, 10, 1))) # numbers for the first row, with unshuffled columns
 
-        shuffled = _shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9])
         solution = tuple(
-            shuffled[_apply_pattern(r, c)]
+            seed[pattern(r, c)]
             for c in column_order
             for r in row_order
         )
         return solution
+
+    def _shuffle(self, numbers: list[int]) -> list[int]:
+        """Shuffles the order of elements in a list. Only used for integers, but would work with other types"""
+
+        return random.sample(numbers, len(numbers))
 
 
 class BoardGenerator:
