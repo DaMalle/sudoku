e021333b8dcdcb6f10c916a9af00db125fa39c64
Damalle <marcfriis@protonmail.com>
2025-06-17 23:46:19 +0200

    updated sudokusolution-test to fit 2d tuple

--- test/test_model.py
+++ test/test_model.py
@@ -11,8 +11,13 @@ class TestSolutionGenerator(unittest.TestCase):
         self.logger.info("running: test_format")
 
         self.assertIsInstance(self.solution, tuple)
-        self.assertEqual(len(self.solution), 81)
-        self.assertTrue(all(1 <= x <= 9 for x in self.solution))
+        self.assertTrue(all(isinstance(row, tuple) for row in self.solution))
+
+
+        self.assertEqual(len(self.solution), 9)
+        self.assertTrue(all(len(row)==9 for row in self.solution))
+
+        self.assertTrue(all(1 <= x <= 9 for row in self.solution for x in row))
 
     def test_deterministic_with_seed(self):
         self.logger.info("running: test_deterministic_with_seed")
@@ -30,7 +35,7 @@ class TestSolutionGenerator(unittest.TestCase):
         for i in range(9):
             with self.subTest(i=i):
                 self.logger.info(f"running: test_is_valid_row: index{i}")
-                self.assertEqual(set(self.solution[i*9:i*9+9]), set(range(1, 10, 1)))
+                self.assertEqual(set(self.solution[i]), set(range(1, 10, 1)))
 
     def test_is_valid_column(self):
         self.logger.info("running: test_is_valid_column")
@@ -38,7 +43,7 @@ class TestSolutionGenerator(unittest.TestCase):
         for i in range(9):
             with self.subTest(i=i):
                 self.logger.info(f"running: test_is_valid_column: index{i}")
-                self.assertEqual(set(self.solution[i*9+j] for j in range(9)), set(range(1, 10, 1)))
+                self.assertEqual(set(self.solution[i][j] for j in range(9)), set(range(1, 10, 1)))
 
     def test_is_valid_box(self):
         self.logger.info("running: test_is_valid_box")
@@ -46,8 +51,9 @@ class TestSolutionGenerator(unittest.TestCase):
         for i in range(9):
             with self.subTest(i=i):
                 self.logger.info(f"running: test_is_valid_box: index{i}")
+                x0 = ()
                 self.assertEqual(
-                    set(self.solution[((i // 3) * 3 + k) * 9 + ((i % 3) * 3 + j)]
+                    set(self.solution[((i // 3) * 3 + k)][((i % 3) * 3 + j)]
                         for k in range(3)
                         for j in range(3)),
                     set(range(1, 10, 1))
